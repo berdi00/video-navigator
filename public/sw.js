31c822707f225d422c6f34b941da6acca39ce2b3
Berdimyrat <berdinazarov731@gmail.com>
2025-04-27 16:01:09 +0500

    fix: sw load

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,7 @@
-import { videoLinks } from '@/data';
-import { filterVideos } from '../src/utils/filteringVideos';
-
 self.addEventListener('install', event => {
-	event.waitUntil(caches.open('v1').then(cache => cache.addAll(filterVideos(videoLinks))));
+	event.waitUntil(caches.open('v1').then(cache => cache.addAll(['/index.html']))); // Try caching a basic asset
 });
 
 self.addEventListener('fetch', event => {
-	event.respondWith(
-		caches.match(event.request).then(response => {
-			// caches.match() always resolves
-			// but in case of success response will have value
-			if (response !== undefined) {
-				return response;
-			} else {
-				return fetch(event.request)
-					.then(response => {
-						// response may be used only once
-						// we need to save clone to put one copy in cache
-						// and serve second one
-						const responseClone = response.clone();
-
-						caches.open('v1').then(cache => {
-							cache.put(event.request, responseClone);
-						});
-						return response;
-					})
-					.catch(() => caches.match('/video/offline.mp4'));
-			}
-		})
-	);
+	event.respondWith(caches.match(event.request) || fetch(event.request));
 });
