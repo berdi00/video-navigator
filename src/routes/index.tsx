07f83a1a017704d341c75e22fa7423ef87ebedcc
Berdimyrat <berdinazarov731@gmail.com>
2025-04-27 17:49:34 +0500

    feat: video player

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, useRouter } from '@tanstack/react-router';
 import { useState } from 'react';
 
 export const Route = createFileRoute('/')({
@@ -7,17 +7,17 @@ export const Route = createFileRoute('/')({
 
 function Index() {
 	const [loading, setLoading] = useState(false);
-	const [init, setInit] = useState(false);
-	const [isRegistered, setIsRegistered] = useState(false);
+	const router = useRouter();
+
 	const registerWorker = async () => {
 		if ('serviceWorker' in navigator) {
-			setInit(true);
 			setLoading(true);
 			navigator.serviceWorker
 				.register('/sw.js', { scope: '/' })
 				.then(registration => {
-					console.log('Service Worker registered with scope:', registration.scope);
-					setIsRegistered(true);
+					if (registration.scope) {
+						router.navigate({ href: '/player' });
+					}
 				})
 				.catch(error => {
 					console.error('Service Worker registration failed:', error);
@@ -30,13 +30,9 @@ function Index() {
 
 	return (
 		<div className='home'>
-			{loading && <h1>Loading...</h1>}
-			<button onClick={registerWorker} className='start_button'>
-				Start
+			<button disabled={loading} onClick={registerWorker} className='start_button'>
+				{loading ? 'Loading' : 'Start'}
 			</button>
-			{!loading && init && (
-				<h3>{isRegistered ? 'Successfully Registered worker' : 'Failure in registering worker'}</h3>
-			)}
 		</div>
 	);
 }
--- src/routes/player.tsx
+++ src/routes/player.tsx
@@ -1,9 +1,43 @@
+import { videoLinks } from '@/data';
+import { filterVideos } from '@/utils/filteringVideos';
 import { createFileRoute } from '@tanstack/react-router';
-
+import { useEffect, useState } from 'react';
+import { MouseEvent as ReactMouseEvent } from 'react';
 export const Route = createFileRoute('/player')({
 	component: Player,
 });
 
 function Player() {
-	return <div className='player'>Hello from Player!</div>;
+	const [videos, setVideos] = useState<string[]>([]);
+	const [currentVideo, setCurrentVideo] = useState(0);
+	const [videoLoading, setVideoLoading] = useState(false);
+	useEffect(() => {
+		setVideos(filterVideos(videoLinks));
+	}, []);
+
+	const handlePrevNext = (index: number, e: ReactMouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		if ((currentVideo === 0 && index < 0) || (currentVideo === videos.length - 1 && index > 0)) {
+			return;
+		}
+		setCurrentVideo(prev => prev + index);
+	};
+
+	return (
+		<div className='player'>
+			<div className='video-container'>
+				<video
+					onLoadStart={() => setVideoLoading(true)}
+					onLoadedData={() => setVideoLoading(false)}
+					autoPlay
+					src={videos[currentVideo]}
+				></video>
+			</div>
+			{videoLoading && <div className='loader'>Loading</div>}
+			<div className='control_buttons'>
+				<button onClick={e => handlePrevNext(-1, e)}>Prev</button>
+				<button onClick={e => handlePrevNext(1, e)}>Next</button>
+			</div>
+		</div>
+	);
 }
