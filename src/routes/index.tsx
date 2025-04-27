3c5b397dab2953a45ff75f061c2d677919f12ea4
Berdimyrat <berdinazarov731@gmail.com>
2025-04-27 18:22:09 +0500

    feat: navigate to player after activation of sw

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,51 @@
 import { createFileRoute, useRouter } from '@tanstack/react-router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Route = createFileRoute('/')({
 	component: Index,
 });
 
 function Index() {
-	const [loading, setLoading] = useState(false);
+	const [swReady, setSwReady] = useState(false);
 	const router = useRouter();
 
 	const registerWorker = async () => {
 		if ('serviceWorker' in navigator) {
-			setLoading(true);
-			navigator.serviceWorker
-				.register('/sw.js', { scope: '/' })
-				.then(registration => {
-					if (registration.scope) {
-						router.navigate({ href: '/player' });
-					}
-				})
-				.catch(error => {
-					console.error('Service Worker registration failed:', error);
-				})
-				.finally(() => {
-					setLoading(false);
+			try {
+				const registration = await navigator.serviceWorker.register('/sw.js', {
+					scope: '/',
 				});
+
+				// Listen for the activate event
+				registration.addEventListener('statechange', () => {
+					if (registration.active?.state === 'activated') {
+						console.log('Service Worker is activated!');
+						setSwReady(true); // Set state when activated
+					}
+				});
+
+				// If it's already active, set to ready
+				if (registration.active?.state === 'activated') {
+					setSwReady(true);
+				}
+
+				console.log('Service worker registered successfully:', registration);
+			} catch (error) {
+				console.error('Service worker registration failed:', error);
+			}
 		}
 	};
 
+	useEffect(() => {
+		if (swReady) {
+			router.navigate({ href: '/player' });
+		}
+	}, [router, swReady]);
+
 	return (
 		<div className='home'>
-			<button disabled={loading} onClick={registerWorker} className='start_button'>
-				{loading ? 'Loading' : 'Start'}
+			<button disabled={swReady} onClick={registerWorker} className='start_button'>
+				{swReady ? 'Loading' : 'Start'}
 			</button>
 		</div>
 	);
