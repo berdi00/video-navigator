2ce1d735157b262fbde55e1e7193c867ed5c0cfc
Berdimyrat <berdinazarov731@gmail.com>
2025-04-27 15:12:28 +0500

    fix: sw load

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
-// import registerServiceWorker from './utils/register';
+import registerServiceWorker from './utils/register';
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -18,18 +18,7 @@ declare module '@tanstack/react-router' {
 }
 
 // Register the service worker if it's not already registered
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker
-			.register('/sw.js') // Ensure this path points to the correct location of your sw.js
-			.then(registration => {
-				console.log('Service Worker registered with scope:', registration.scope);
-			})
-			.catch(error => {
-				console.error('Service Worker registration failed:', error);
-			});
-	});
-}
+registerServiceWorker();
 
 // Render the app
 const rootElement = document.getElementById('root')!;
--- src/utils/register.ts
+++ src/utils/register.ts
@@ -1,15 +1,19 @@
 const registerServiceWorker = async () => {
 	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', () => {
-			navigator.serviceWorker
-				.register('/sw.js') // Ensure this path points to the correct location of your sw.js
-				.then(registration => {
-					console.log('Service Worker registered with scope:', registration.scope);
-				})
-				.catch(error => {
-					console.error('Service Worker registration failed:', error);
-				});
-		});
+		try {
+			const registration = await navigator.serviceWorker.register('sw.js', {
+				scope: './',
+			});
+			if (registration.installing) {
+				console.log('Service worker installing');
+			} else if (registration.waiting) {
+				console.log('Service worker installed');
+			} else if (registration.active) {
+				console.log('Service worker active');
+			}
+		} catch (error) {
+			console.error(`Registration failed with ${error}`);
+		}
 	}
 };
 export default registerServiceWorker;
